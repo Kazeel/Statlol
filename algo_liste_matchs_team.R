026e4acbc051fb61fb0ca6165a1c37b04a14ac61
Kazeel <kazeel@hotmail.fr>
2017-09-19 19:00:46 +0200

    Algo de récupération d'Id des matchs
    Bon, pour le moment c'est juste du code, mais j'ai pu récupérer la liste des matchs, maintenant, il va falloir automatiser tout cela en fonction, mais ce sera pour un peu après

--- algo_liste_matchs_team.R
+++ algo_liste_matchs_team.R
@@ -23,6 +23,7 @@ key    <- "RGAPI-ea3a2952-91ac-48be-96c0-0bcd83f505a9"
 #
 #
 ############################
+
 pseudo_top <- "RKSReidoz"
 pseudo_jun <- "Kazeel"
 pseudo_mid <- "Mashu"
@@ -50,4 +51,26 @@ for(i in 1:5){
 
 data.pseudo_id <- data.frame(pseudos_joueurs, ids_joueurs)
 
-write.csv(data.pseudo_id, file = "ids_joueurs.csv") #Enregristrement
+write.csv(data.pseudo_id, file = "ids_joueurs.csv") #Enregistrement
+
+###########################
+# Récupération de la liste des matchs de chaque joueur
+###########################
+#
+#
+#
+###############################
+
+listes_matchs <- data.frame()
+for(i in 1:5){
+  # Si l'Id est 0 alors vous n'avez pas rentré un pseudo
+  id.loop <- data.pseudo_id[i,"ids_joueurs"]
+  pseudo.loop <- data.pseudo_id[i,"pseudos_joueurs"]
+  if(id.loop != 0 ){
+    listes_matchs<- rbind(listes_matchs,lol.matchslist.ranked.easy(id.loop, serveur, key)[[1]])
+  } 
+}
+
+flex.listes_matchs <- subset(listes_matchs, queue == 440)
+team.games <- flex.listes_matchs[duplicated(flex.listes_matchs$gameId, fromLast = TRUE),c(1,2,5,6)]
+write.csv(team.games, file = "liste_game_team.csv") #Enregistrement
