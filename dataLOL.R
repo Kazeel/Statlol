577fd600af552345f7500e0c881cdb75aa1ff24c
CokeAleInn <32029606+CokeAleInn@users.noreply.github.com>
2017-10-04 19:20:05 +0200

    Ajout de la class Game dans le script

--- dataLOL.R
+++ dataLOL.R
@@ -5,7 +5,7 @@ matchinfo<-read.csv("matchs_stats.csv")
 names(matchinfo)
 matchinfo<-matchinfo[c(1:65,79:87)]
 matchinfo<-matchinfo[c(1:5,13:74)]
-Donnees<-(data.frame(Id=matchinfo$X,
+Data<-data.frame(Id=matchinfo$X,
                      Id_Part= matchinfo$participantId,
                      Id_Team=matchinfo$teamId,
                      Win=matchinfo$win,
@@ -26,5 +26,26 @@ Donnees<-(data.frame(Id=matchinfo$X,
                      F_Tower=matchinfo$firstTowerKill,
                      Id_Ac=matchinfo$accountId,
                      Id_Sum=matchinfo$summonerId,
-                     Duree=matchinfo$duration))
+                     Duree=matchinfo$duration,
+                     Id_game=substr(matchinfo$X,1,10))
 
+Data$Id_game<-levels(Data$Id_game)#A corriger !
+
+
+Game<-function(Id,etat,duree){
+  #Création et constructeur de la classe Game qui est définie par un id (unique), un état et une durée
+  Constructeur<-data.frame(etat=etat,duree=duree)
+  Constructeur$Id<-Id
+  class(Constructeur)<-"Game"
+  Constructeur
+}
+G1<-Game(Data$Id_game[1],Data$Win[1],Data$Duree[1])
+print.Game<-function(game){
+  cat("La Game",game$Id,"a durée",game$duree,"s \n")#A Corriger.
+}
+G1
+
+#Transformer Une variable en Factor pour la variable Win
+Test<-factor(Data$Win)
+attributes(Test)$levels<-c("Perdu","Gagné")
+Test
