cbd9dbff93ea0e073dac0a2a6b32121cd914110a
Kazeel <kazeel@hotmail.fr>
2017-10-25 15:12:11 +0200

    creation of the class player in S4

--- lol_class_player.R
+++ lol_class_player.R
@@ -17,12 +17,13 @@
 setClass(
   Class = "Player",
   representation = representation(
-    profileIconId = "numeric",
+    profileIconId = "character",
     name = "character",
     summonerLevel= "numeric",
     accountId = "character",
     id =  "character",
-    revisionDate = "numeric"
+    revisionDate = "character",
+    server = "character"
   )
 )
 ###############################################################################
@@ -30,14 +31,15 @@ setClass(
 ##############################################################################
 setMethod("print","Player",
           function(x,...){
-            cat("*** Class player, method Print ***\n")
+            cat("*** Class Player, method Print ***\n")
             cat("* profileIconId = ");print(x@profileIconId)
             cat("* name = ");print(x@name)
             cat("* summonerLevel= ");print(x@summonerLevel)
             cat("* accountId= ");print(x@accountId)
             cat("* id= ");print(x@id)
             cat("* revisionDate= ");print(x@revisionDate)
-            cat("***** Fin Print(trajectoires) *****\n")
+            cat("* revisionDate= ");print(x@server)
+            cat("***** Fin Print(Player) *****\n")
           }
 )
 
@@ -50,6 +52,7 @@ setMethod("show","Player",
             cat("* accountId= ");print(object@accountId)
             cat("* id= ");print(object@id)
             cat("* revisionDate= ");print(object@revisionDate)
+            cat("* revisionDate= ");print(object@server)
             cat("***** Fin Print(trajectoires) *****\n")
           }
 )
@@ -57,25 +60,73 @@ setMethod("show","Player",
 # Constructors
 ########################################################################
 
-setGeneric(name = "player", def = function (name, accountId, id, ...) {standardGeneric("player")})
+setGeneric(name = "player", def = function (name, accountId, id, apikey, server, ...) {standardGeneric("player")})
 
 setMethod(
   f = "player", 
-  signature = signature(name = "character", accountId = "missing", id = "missing"),
-  definition = function (name) {
-    # TODO
+  signature = signature(name = "character", accountId = "missing", id = "missing", apikey="character", server = "character"),
+  definition = function (name,apikey, server) {
+    # return a Player Class-Object, or a string with the error message
+    
+    #Transformation of data
+    pseudo<- tolower(name)
+    pseudo<- gsub(" ", "", pseudo, fixed = TRUE)
+    
+    #Retriving from api
+    url<-paste("https://",server,".api.riotgames.com/lol/summoner/v3/summoners/by-name/",pseudo,"?api_key=",apikey,sep="")
+    liste<- content(GET(url), as="parsed")
+    
+    #Handle error type
+    try(if ( "status" %in% names(liste) ){
+      msg <- paste(liste$status$message,", error :",liste$status$status_code)
+      warning(msg)
+      return(msg)
+    } )
+    
+    #Generate Player if everything is fine
+    obj <- new(Class="Player",
+               profileIconId = as.character(liste$profileIconId),
+               name = liste$name,
+               summonerLevel= liste$summonerLevel,
+               accountId = as.character(liste$accountId),
+               id =  as.character(liste$id),
+               revisionDate = as.character(liste$revisionDate),
+               server = server)
+    return(obj)
 })
 
 setMethod(
   f = "player", 
-  signature = signature(name = "missing", accountId = "character", id = "missing"),
-  definition = function (name) {
-    # TODO
+  signature = signature(name = "missing", accountId = "character", id = "missing", apikey="character", server = "character"),
+  definition = function (accountId, apikey, server) {
+    # return a Player Class-Object, or a string with the error message
+    
+    #Retriving from api
+    url<-paste("https://",server,".api.riotgames.com/lol/summoner/v3/summoners/by-account/",accountId,"?api_key=",apikey,sep="")
+    liste<- content(GET(url), as="parsed")
+    
+    #Handle error type
+    try(if ( "status" %in% names(liste) ){
+      msg <- paste(liste$status$message,", error :",liste$status$status_code)
+      warning(msg)
+      return(msg)
+    } )
+    
+    #Generate Player if everything is fine
+    obj <- new(Class="Player",
+               profileIconId = as.character(liste$profileIconId),
+               name = liste$name,
+               summonerLevel= liste$summonerLevel,
+               accountId = as.character(liste$accountId),
+               id =  as.character(liste$id),
+               revisionDate = as.character(liste$revisionDate),
+               server = server)
+    return(obj)
   })
 
 setMethod(
   f = "player", 
-  signature = signature(name = "missing", accountId = "missing", id = "character"),
-  definition = function (name) {
+  signature = signature(name = "missing", accountId = "missing", id = "character", apikey="character", server = "character"),
+  definition = function (id, apikey, server) {
     # TODO
   })
